782c2736d8eeb8d7f95588232dbf8c8be106d0fa
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2023-04-02 15:13:38 +0300

    feat: Implement JavaScript functionality (index.js)

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import { StateProvider } from './components/authentication/GoogleStateProvider';
+// import reducer, { initialState } from './components/authentication/googleButton/GoogleReducer';
+
+
+ReactDOM.render(
+        <App />, document.getElementById("root"));
+
